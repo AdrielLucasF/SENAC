7279457ac0f30d79f01758e836d8384ac769dcaf
ADRIEL LUCAS FAGUNDES SILVA <a95069801@rn.senac.local>
2023-09-19 16:57:40 -0300

    exemplo função a fim

---
+++ exemplo_02.py
@@ -0,0 +1,7 @@
+a = 2 
+b = 8
+c = 3 
+x1 = (-b + (b**2 - 4*a*c)**0.5) / (2*a)
+x2 = (-b - (b**2 - 4*a*c)**0.5) / (2*a)
+print("O primeiro zero da função quadrática é:", x1)
+print("O segundo zero da função quadrática é:", x2)
